be6f6cc861b774f93af18e80c9893eb25a514874
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2024-10-22 20:58:18 +0100

    added smooth scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,34 @@
-
+"use client";
 import styles from "./page.module.css";
 import HeroSection from "@/components/heroSection/index";
 import NewsSection from "@/components/newsSection/index";
+import SmoothScrolling from "@/components/smoothScrolling";
 import { Skeleton } from "@mui/material";
 import dynamic from "next/dynamic";
 
-
-const DynamicFooter = dynamic(() => import('@/components/footer'), {
+const DynamicFooter = dynamic(() => import("@/components/footer"), {
   loading: () => (
     <div className={styles.footerSkeleton}>
       <Skeleton variant="rounded" width={"100%"} height={"100%"} />
     </div>
-  )
-})
-
+  ),
+});
 
 const HomePage = () => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div className={styles.firstSection}>
-          <HeroSection />
-        </div>
-        <NewsSection />
-      </main>
-      <footer className={styles.footer}>
-        <DynamicFooter />
-      </footer>
-    </div>
+    <SmoothScrolling>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <div className={styles.firstSection}>
+            <HeroSection />
+          </div>
+          <NewsSection />
+        </main>
+        <footer className={styles.footer}>
+          <DynamicFooter />
+        </footer>
+      </div>
+    </SmoothScrolling>
   );
 };
 
