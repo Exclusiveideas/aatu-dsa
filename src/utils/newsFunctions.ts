0202b5c515840675194e2d77ad5dc6a826a5f572
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2024-10-26 19:35:21 +0100

    buil fix 2.0

--- src/utils/newsFunctions.ts
+++ src/utils/newsFunctions.ts
@@ -1,7 +1,7 @@
 
 import { firestore } from "@/firebase/firebaseConfig";
 import { FetchNewsProps } from "@/types/new";
-import { collection, query, orderBy, limit, startAfter, getDocs } from 'firebase/firestore';
+import { collection, query, limit, startAfter, getDocs } from 'firebase/firestore';
 
 
 
@@ -28,7 +28,7 @@ export const fetchNews = async ({ lastDoc, updateFetchedNews, updateLastDoc, upd
       if(!news[0]) return;
       updateFetchedNews(news);
       updateLastDoc(snapshot.docs[snapshot.docs.length - 1]);
-    } catch () {
+    } catch (_) {
       updateFetchedNewsError('Error fetching news - Try reloading the page.');
     } finally {
       updateIsFetching(false);
